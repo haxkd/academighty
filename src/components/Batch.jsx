e851c14ea28c86bc7207fb4115b093350576898f
Abul Hasan <33129246+haxkd@users.noreply.github.com>
2023-11-26 22:56:48 +0530

    added batch.jsx

--- src/components/Batch.jsx
+++ src/components/Batch.jsx
@@ -1,17 +1,92 @@
-/* eslint-disable no-unused-vars */
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from './includes/Header'
 import Footer from './includes/Footer'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "./includes/Config";
+
 
 const Batch = () => {
-    let params = useParams();
-    
+  let params = useParams();
+  let navigate = useNavigate();
+  let [batch, setBatch] = useState();
+  useEffect(() => {
+    getData();
+  }, []);
+  async function getData() {
+    const docRef = doc(db, "batches", params.id);
+    const docSnap = await getDoc(docRef);
+    const data = await docSnap.data();
+    if (data === undefined) {
+      navigate("/")
+    }
+    setBatch(docSnap.data());
+    console.log(batch);
+  }
   return (
     <>
-    <Header/>
-
-    <Footer/>
+      <Header />
+      <div className="container-fluid py-5">
+    <div className="container py-5">
+      <div className="row">
+        <div className="col-lg-8">
+          <div className="mb-5">
+            <div className="section-title position-relative mb-5">
+              <h6 className="d-inline-block position-relative text-secondary text-uppercase pb-2">
+                Batch Detail
+              </h6>
+              <h1 className="display-4">
+                {batch && batch.BatchName}
+              </h1>
+            </div>
+            <img
+              className="img-fluid rounded w-100 mb-4"
+              src="/img/courses-4.jpg"
+              alt="Images"
+            />
+          </div>
+        
+        </div>
+        <div className="col-lg-4 mt-5 mt-lg-0">
+          <div className="bg-primary mb-5 py-3">
+            <h3 className="text-white py-3 px-4 m-0">Course Features</h3>
+            <div className="d-flex justify-content-between border-bottom px-4">
+              <h6 className="text-white my-3">Instructor</h6>
+              <h6 className="text-white my-3">John Doe</h6>
+            </div>
+            <div className="d-flex justify-content-between border-bottom px-4">
+              <h6 className="text-white my-3">Rating</h6>
+              <h6 className="text-white my-3">
+                4.5 <small>(250)</small>
+              </h6>
+            </div>
+            <div className="d-flex justify-content-between border-bottom px-4">
+              <h6 className="text-white my-3">Lectures</h6>
+              <h6 className="text-white my-3">15</h6>
+            </div>
+            <div className="d-flex justify-content-between border-bottom px-4">
+              <h6 className="text-white my-3">Duration</h6>
+              <h6 className="text-white my-3">10.00 Hrs</h6>
+            </div>
+            <div className="d-flex justify-content-between border-bottom px-4">
+              <h6 className="text-white my-3">Skill level</h6>
+              <h6 className="text-white my-3">All Level</h6>
+            </div>
+            <div className="d-flex justify-content-between px-4">
+              <h6 className="text-white my-3">Language</h6>
+              <h6 className="text-white my-3">English</h6>
+            </div>
+            <div className="py-3 px-4">
+              <a className="btn btn-block btn-secondary py-3 px-5" href="#top">
+                Enroll Now
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+      <Footer />
     </>
   )
 }
